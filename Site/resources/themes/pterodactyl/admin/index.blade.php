4810de66dceee0fa7f1a98bf8499fd318acfaf45
DukeVenator <32488032+DukeVenator@users.noreply.github.com>
2019-05-10 13:18:39 +1200

    Intial
    Comit

---
+++ Site/resources/themes/pterodactyl/admin/index.blade.php
@@ -0,0 +1,58 @@
+{{-- Pterodactyl - Panel --}}
+{{-- Copyright (c) 2015 - 2017 Dane Everitt <dane@daneeveritt.com> --}}
+
+{{-- This software is licensed under the terms of the MIT license. --}}
+{{-- https://opensource.org/licenses/MIT --}}
+@extends('layouts.admin')
+
+@section('title')
+    Administration
+@endsection
+
+@section('content-header')
+    <h1>Administrative Overview<small>A quick glance at your system.</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('admin.index') }}">Admin</a></li>
+        <li class="active">Index</li>
+    </ol>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box
+            @if($version->isLatestPanel())
+                box-success
+            @else
+                box-danger
+            @endif
+        ">
+            <div class="box-header with-border">
+                <h3 class="box-title">System Information</h3>
+            </div>
+            <div class="box-body">
+                @if ($version->isLatestPanel())
+                    You are running the following version <code>{{ config('app.version') }}</code>. Your panel is up-to-date! Yay the boss won't kill me now!
+                @else
+                    Your panel is <strong>GASP! Out of date!</strong> Please contact support to have it updated
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-6 col-sm-3 text-center">
+        <a href="https://discord.gg/K4Ggvqz"><button class="btn btn-warning" style="width:100%;"><i class="fa fa-fw fa-support"></i> Get Help <small>(via Discord)</small></button></a>
+    </div>
+    <div class="col-xs-6 col-sm-3 text-center">
+        <a href="https://docs.pterodactyl.io"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-link"></i> Documentation</button></a>
+    </div>
+    <div class="clearfix visible-xs-block">&nbsp;</div>
+    <div class="col-xs-6 col-sm-3 text-center">
+        <a href="https://github.com/Pterodactyl/Panel"><button class="btn btn-primary" style="width:100%;"><i class="fa fa-fw fa-support"></i> Github</button></a>
+    </div>
+    <div class="col-xs-6 col-sm-3 text-center">
+        <a href="https://donorbox.org/pterodactylssss"><button class="btn btn-success" style="width:100%;"><i class="fa fa-fw fa-money"></i> Support the Servers</button></a>
+    </div>
+</div>
+@endsection
