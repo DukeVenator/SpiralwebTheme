ef2904562b589dc7feb773ddd3ce055c33b3d329
DukeVenator <32488032+DukeVenator@users.noreply.github.com>
2019-05-10 13:37:31 +1200

    Update build.sh

--- Site/build.sh
+++ Site/build.sh
@@ -22,8 +22,8 @@ if [ ! -f "artisan" ]; then
 
     echo "Downloading the Theme you picked"
     mkdir -p tempdown && cd tempdown && git clone https://github.com/DukeVenator/SpiralwebTheme.git .
-    cp -r site/public ..
-    cp -r site/resources ..
+    cp -r Site/public ..
+    cp -r Site/resources ..
 
     echo "Files have been copied over!"
     sleep 2
