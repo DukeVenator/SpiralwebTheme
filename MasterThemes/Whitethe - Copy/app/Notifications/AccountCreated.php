2c2ced6d6b288f8ec44f476ec0a05cf0cd45f0e4
DukeVenator <32488032+DukeVenator@users.noreply.github.com>
2019-05-16 00:24:47 +1200

    Email fixes

--- MasterThemes/Whitethe - Copy/app/Notifications/AccountCreated.php
+++ MasterThemes/Whitethe - Copy/app/Notifications/AccountCreated.php
@@ -62,7 +62,8 @@ class AccountCreated extends Notification implements ShouldQueue
             ->greeting('Greetings ' . $this->user->name . '!')
             ->line('You are receiving this email from our minions because an account has been created for you on ' . config('app.name') . '.')
             ->line('Username: ' . $this->user->username)
-            ->line('Email: ' . $this->user->email);
+            ->line('Email: ' . $this->user->email)
+            ->line('Please consider replying to this email to help us build you a better experience.. also to let us know this works ;)');
 
         if (! is_null($this->token)) {
             return $message->action('Setup Your Account', url('/auth/password/reset/' . $this->token . '?email=' . urlencode($this->user->email)));
